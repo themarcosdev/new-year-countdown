1d42ff12b0cae1b99c1ec892e288aeb57d38ecfc
themarcosdev <>
2021-07-30 08:39:27 +0000

    projetocriado

---
+++ README.md
@@ -0,0 +1,3 @@
+:brazil: Um contador regressivo web simples até o fim de ano<br> 
+|<br/>
+:us: A simple web countdown to the end of the year 
---
+++ script.js
@@ -0,0 +1,36 @@
+// as const com El se referem as que vou pegar usando .getElementById
+const diasEl = document.getElementById("dias");
+const horasEl = document.getElementById("horas");
+const minutosEl = document.getElementById("minutos");
+const segundosEl = document.getElementById("segundos");
+
+const anoNovo = "1 Jan 2022";
+
+//definindo uma função para def dias,hs,min e seg.
+function contadorRegressivo() {
+    const dataAnoNovo = new Date(anoNovo);
+    const dataAtual = new Date();
+
+    const totalSegundos = (dataAnoNovo - dataAtual) / 1000;
+
+    const dias = Math.floor(totalSegundos/ 3600 / 24);
+    const horas = Math.floor(totalSegundos / 3600) % 24;
+    const minutos = Math.floor(totalSegundos / 60) % 60;
+    const segundos = Math.floor(totalSegundos) % 60;
+
+    //declarando que as varEl irão receber um novo valor via innerHTML
+    diasEl.innerHTML = dias;
+    horasEl.innerHTML = formatarTempo(horas);
+    minutosEl.innerHTML = formatarTempo(minutos);
+    segundosEl.innerHTML = formatarTempo(segundos);
+}
+
+// formatando o tempo
+function formatarTempo(tempo) {
+    return tempo < 10 ? `0${tempo}` : tempo;
+}
+
+// chamada inicial do contador e def intervalo.
+  contadorRegressivo();
+
+  setInterval(contadorRegressivo, 1000);
