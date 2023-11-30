a078aa003f237c12d03d555288a601f3c72a782c
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-11-29 22:51:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-// as const com El se referem as que vou pegar usando .getElementById
-const diasEl = document.getElementById("dias");
-const horasEl = document.getElementById("horas");
-const minutosEl = document.getElementById("minutos");
-const segundosEl = document.getElementById("segundos");
+    const diasEl = document.getElementById("dias");
+    const horasEl = document.getElementById("horas");
+    const minutosEl = document.getElementById("minutos");
+    const segundosEl = document.getElementById("segundos");
 
-const anoNovo = "1 Jan 2024";
+    const anoNovo = `1 Jan ${new Date().getFullYear() + 1}`;
 
-//definindo uma função para def dias,hs,min e seg.
-function contadorRegressivo() {
-    const dataAnoNovo = new Date(anoNovo);
-    const dataAtual = new Date();
+    function contadorRegressivo() {
+        const dataAnoNovo = new Date(anoNovo);
+        const dataAtual = new Date();
 
-    const totalSegundos = (dataAnoNovo - dataAtual) / 1000;
+        const totalSegundos = (dataAnoNovo - dataAtual) / 1000;
 
-    const dias = Math.floor(totalSegundos/ 3600 / 24);
-    const horas = Math.floor(totalSegundos / 3600) % 24;
-    const minutos = Math.floor(totalSegundos / 60) % 60;
-    const segundos = Math.floor(totalSegundos) % 60;
+        const dias = Math.floor(totalSegundos/ 3600 / 24);
+        const horas = Math.floor(totalSegundos / 3600) % 24;
+        const minutos = Math.floor(totalSegundos / 60) % 60;
+        const segundos = Math.floor(totalSegundos) % 60;
 
-    //declarando que as varEl irão receber um novo valor via innerHTML
-    diasEl.innerHTML = dias;
-    horasEl.innerHTML = formatarTempo(horas);
-    minutosEl.innerHTML = formatarTempo(minutos);
-    segundosEl.innerHTML = formatarTempo(segundos);
-}
+        diasEl.innerHTML = dias;
+        horasEl.innerHTML = formatarTempo(horas);
+        minutosEl.innerHTML = formatarTempo(minutos);
+        segundosEl.innerHTML = formatarTempo(segundos);
+    }
 
-// formatando o tempo
-function formatarTempo(tempo) {
-    return tempo < 10 ? `0${tempo}` : tempo;
-}
+    function formatarTempo(tempo) {
+        return tempo < 10 ? `0${tempo}` : tempo;
+    }
 
-// chamada inicial do contador e def intervalo.
-  contadorRegressivo();
+    contadorRegressivo();
 
-  setInterval(contadorRegressivo, 1000);
+    setInterval(contadorRegressivo, 1000);
+    
